68505ac6ce7c74eba3bb72d28cb5101107558a95
Abhishekgitok <abhishek19781@gmail.com>
2024-03-12 19:03:50 +0530

    minnor

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function App  (){
   return (
     <>
-      <h1>this is is heading </h1>
+    
+      <div className=' flex mx-auto justify-around mt-4'>
+        <div className='h-16 w-16'>
+           <img src="logo.png" alt="" /> 
+        </div>
+        <div className=''>
+          <ul className='flex  gap-7'>
+          <li>Home</li>
+          <li>services</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
